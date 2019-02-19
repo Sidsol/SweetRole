d491cf35fe0dbc405d0880ab5197dc9eb43e8a3a
Sidsol <sidsol@live.com>
2019-02-19 15:10:20 -0500

    fixed styling on many pages

---
+++ SweetRole/ViewModels/DetailsStoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SweetRole.Areas.Identity.Data;
+using SweetRole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SweetRole.ViewModels
+{
+    public class DetailsStoryViewModel
+    {
+        public List<SelectListItem> Users { get; set; }
+        public Story Story { get; set; }
+        public DetailsStoryViewModel() { }
+
+        public DetailsStoryViewModel(Story story, IEnumerable<SweetRoleUser> users)
+        {
+            Users = new List<SelectListItem>();
+            foreach (var user in users)
+            {
+                Users.Add(new SelectListItem
+                {
+                    Value = user.Id.ToString(),
+                    Text = user.UserName
+                });
+            }
+            Story = story;
+        }
+    }
+}
